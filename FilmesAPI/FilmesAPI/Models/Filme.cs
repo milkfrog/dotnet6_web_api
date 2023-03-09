6a7d242464ab100d5ad665508a3a407a02febda7
Marcelo Contin <contin.marcelo@gmail.com>
2023-03-08 22:19:44 -0500

    Add files from course

---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O titulo é obrigatório")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O genero é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho máximo é 50 char")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 a 600min")]
+    public int Duracao { get; set; }    
+}
---
+++ FilmesAPI/FilmesAPI/Program.cs
@@ -0,0 +1,32 @@
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
+
+builder.Services.AddDbContext<FilmeContext>(opts =>
+{
+    opts.UseMySql(connectionString,
+        ServerVersion.AutoDetect(connectionString));
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
